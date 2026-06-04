fae55fbd04a4420c75872c12641da6d6813e1122
Lucas <bahlucas@icloud.com>
2026-06-03 23:00:47 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.devicespooflab.hooks"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.spoofmydevice"
@@ -25,8 +25,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     buildFeatures {
